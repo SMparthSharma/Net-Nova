13ba9a035e10c4b2d9121e18f41cba13ba6f51b2
SMparth sharma <darkcodeing0@gmail.com>
2025-06-07 07:53:59 +0530

    add model class

---
+++ lib/model/vpn_infp.dart
@@ -0,0 +1,43 @@
+class VpnInfp {
+  late final String ip;
+  late final String hostName;
+  late final int speed;
+  late final String ping;
+  late final String countryLongname;
+  late final String countryShortname;
+  late final int vpnSessionNum;
+  late final String base64OpenVPNConfigurationData;
+  VpnInfp({
+    required this.hostName,
+    required this.ip,
+    required this.ping,
+    required this.speed,
+    required this.countryLongname,
+    required this.countryShortname,
+    required this.vpnSessionNum,
+    required this.base64OpenVPNConfigurationData,
+  });
+  VpnInfp.fromJson(Map<String, dynamic> jsonData) {
+    hostName = jsonData['HostName'] ?? '';
+    ip = jsonData['IP'] ?? '';
+    ping = jsonData['Ping'] ?? '';
+    speed = jsonData['Speed'] ?? 0;
+    countryLongname = jsonData['CountryLong'] ?? '';
+    countryShortname = jsonData['CountryShort'] ?? '';
+    vpnSessionNum = jsonData['NumVpnSession'] ?? 0;
+    base64OpenVPNConfigurationData =
+        jsonData['OpenVpn_ConfigData_Base64'] ?? '';
+  }
+  Map<String, dynamic> toJson() {
+    final jsonData = <String, dynamic>{};
+    jsonData['HostName'] = hostName;
+    jsonData['IP'] = ip;
+    jsonData['Ping'] = ping;
+    jsonData['Speed'] = speed;
+    jsonData['CountryLong'] = countryLongname;
+    jsonData['CountryShort'] = countryShortname;
+    jsonData['NumVpnSession'] = vpnSessionNum;
+    jsonData['OpenVpn_ConfigData_Base64'] = base64OpenVPNConfigurationData;
+    return jsonData;
+  }
+}
